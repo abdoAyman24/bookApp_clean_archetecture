b66d19cd26dc16e62a2ee2570381a4023b7ad82d
abdelftah <abdelftahayman77@gmail.com>
2025-11-09 01:18:56 +0200

    add hive in test

--- test/book_api_test.dart
+++ test/book_api_test.dart
@@ -1,18 +1,29 @@
+import 'dart:io';
+
 import 'package:book_app_clean_archetecture/Features/home/data/data_source/home_remote_data_source.dart';
+import 'package:book_app_clean_archetecture/constants.dart';
 import 'package:book_app_clean_archetecture/core/utils/api_service.dart';
 import 'package:book_app_clean_archetecture/Features/home/domain/Entities/entities.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
 
 import 'book_api_test.mocks.dart';
 
 @GenerateMocks([ApiService])
-void main() {
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+ final tempDir = Directory.systemTemp.createTempSync();
+  Hive.init(tempDir.path);
+  if (!Hive.isAdapterRegistered(0)) {
+    Hive.registerAdapter(EntitiesBooksAdapter());
+  }
+  await Hive.openBox<EntitiesBooks>(KFuturedBox);
+  await Hive.openBox<EntitiesBooks>(KNewsBox);
   late HomeRemoteDataSourceImpl dataSource;
   late MockApiService mockApiService;
-
   setUp(() {
     mockApiService = MockApiService();
     dataSource = HomeRemoteDataSourceImpl(mockApiService);
@@ -27,7 +38,7 @@ void main() {
             "title": "Clean Code",
             "authors": ["Robert C. Martin"],
             "imageLinks": {"thumbnail": "https://example.com/image1.jpg"},
-          }
+          },
         },
         {
           "id": "book2",
@@ -35,9 +46,9 @@ void main() {
             "title": "Flutter for Beginners",
             "authors": ["John Doe"],
             "imageLinks": {"thumbnail": "https://example.com/image2.jpg"},
-          }
-        }
-      ]
+          },
+        },
+      ],
     };
 
     test('fetchFutureBooks returns list of EntitiesBooks', () async {
